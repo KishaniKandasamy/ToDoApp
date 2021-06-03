a7d6124e78375bae3fac25b6c371fdb2d0ff193b
KishaKandha <kishanik1118@gmail.com>
2021-06-03 20:36:34 +0530

    create add todo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import { useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take rubbish out']);
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take rubbish out', 'Complete the online courses']);
+  const [input, setInput] = useState('');
+  console.log(input);
+
+  const addTodo = (event) => {
+    event.preventDefault(); //will stop the refreshing
+    console.log("Im working!");
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input />
-      <button>Add ToDo</button>
+      <form>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button type="submit" onClick={addTodo} >Add ToDo</button>
 
       <ul>
-        <li></li>
-        <li></li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
-
+      </form>
     </div>
   );
 }
