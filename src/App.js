8657435becc95a8257cef268522ad48ee2fa857d
KishaKandha <kishanik1118@gmail.com>
2021-06-03 15:02:05 +0530

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take rubbish out']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <input />
+      <button>Add ToDo</button>
+
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+
     </div>
   );
 }
